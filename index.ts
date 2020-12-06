054fff8eeae2b1ee9acca6250c17b7c9aae86dc5
Jondolf <57632562+Jondolf@users.noreply.github.com>
2020-12-06 15:15:11 +0200

    Add index.ts file for running all solutions

---
+++ index.ts
@@ -0,0 +1,18 @@
+import { solveDay1Part1 } from "./solutions/day1";
+import { solveDay2Part1 } from "./solutions/day2";
+import { solveDay3Part1 } from "./solutions/day3";
+import { solveDay4Part1 } from "./solutions/day4";
+import { solveDay5Part1 } from "./solutions/day5";
+import { solveDay6Part1 } from "./solutions/day6";
+
+// The function calls simply run the files
+function main() {
+  solveDay1Part1();
+  solveDay2Part1();
+  solveDay3Part1();
+  solveDay4Part1();
+  solveDay5Part1();
+  solveDay6Part1();
+}
+
+main();
